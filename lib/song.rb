bf0548dbcfd04d81f5cd0babadf6109d9a1497ab
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-27 18:37:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     @@all 
   end 
   
-  def self.new_by_filename
+  def self.new_by_filename(song_name)
     p song_name 
     artist, song = song_name.split(' - ')
     artist = Artist.new(artist)
@@ -20,8 +20,8 @@ class Song
     song 
   end 
   
-  def artist_name(name)
-    self.artist = Artist.find_or_create_by_name
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
   end 
   
 end 
